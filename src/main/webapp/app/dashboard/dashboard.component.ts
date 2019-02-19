eeed673db8389df96013d190fe7b5dfca61cb0ea
Theo LEBRUN <mib.ftw@gmail.com>
2019-02-19 18:27:29 -0500

    Add alert

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -3,15 +3,36 @@ import {Inject, Vue} from 'vue-property-decorator';
 import DashboardService from '@/dashboard/dashboard.service';
 import {IBankAccount} from '@/shared/model/bank-account.model';
 import {IOperation} from '@/shared/model/operation.model';
+import AlertService from '@/shared/alert/alert.service';
 
 @Component
 export default class Dashboard extends Vue {
   @Inject('dashboardService') private dashboardService: () => DashboardService;
+  @Inject('alertService') private alertService: () => AlertService;
   public bankAccounts: IBankAccount[] = [];
   public operations: IOperation[] = [];
 
+  public dismissCountDown: number = this.$store.getters.dismissCountDown;
+  public dismissSecs: number = this.$store.getters.dismissSecs;
+  public alertType: string = this.$store.getters.alertType;
+  public alertMessage: any = this.$store.getters.alertMessage;
+
+  public getAlertFromStore() {
+    this.dismissCountDown = this.$store.getters.dismissCountDown;
+    this.dismissSecs = this.$store.getters.dismissSecs;
+    this.alertType = this.$store.getters.alertType;
+    this.alertMessage = this.$store.getters.alertMessage;
+  }
+
+  public countDownChanged(dismissCountDown: number) {
+    this.alertService().countDownChanged(dismissCountDown);
+    this.getAlertFromStore();
+  }
+
   public mounted(): void {
     this.retrieveDashboard();
+    this.alertService().showAlert('This is an error message', 'danger');
+    this.getAlertFromStore();
   }
 
   public retrieveDashboard(): void {
