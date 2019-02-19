7ac5852cad64e23a27edd8eee7d5077b586e0834
Theo LEBRUN <mib.ftw@gmail.com>
2019-02-19 11:41:59 -0500

    Entities generation

--- src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/myapp/config/CacheConfiguration.java
@@ -38,6 +38,12 @@ public class CacheConfiguration {
             cm.createCache(com.mycompany.myapp.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.mycompany.myapp.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.mycompany.myapp.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.BankAccount.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.BankAccount.class.getName() + ".operations", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Label.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Label.class.getName() + ".operations", jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Operation.class.getName(), jcacheConfiguration);
+            cm.createCache(com.mycompany.myapp.domain.Operation.class.getName() + ".labels", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
---
+++ src/test/javascript/spec/app/entities/bank-account/bank-account-detail.component.spec.ts
@@ -0,0 +1,55 @@
+/* tslint:disable max-line-length */
+import { shallowMount, createLocalVue, Wrapper } from '@vue/test-utils';
+import axios from 'axios';
+
+import * as config from '@/shared/config/config';
+import BankAccountDetailComponent from '@/entities/bank-account/bank-account-details.vue';
+import BankAccountClass from '@/entities/bank-account/bank-account-details.component';
+import BankAccountService from '@/entities/bank-account/bank-account.service';
+
+const localVue = createLocalVue();
+const mockedAxios: any = axios;
+
+config.initVueApp(localVue);
+const i18n = config.initI18N(localVue);
+const store = config.initVueXStore(localVue);
+localVue.component('font-awesome-icon', {});
+localVue.component('router-link', {});
+
+jest.mock('axios', () => ({
+  get: jest.fn()
+}));
+
+describe('Component Tests', () => {
+  describe('BankAccount Management Detail Component', () => {
+    let wrapper: Wrapper<BankAccountClass>;
+    let comp: BankAccountClass;
+
+    beforeEach(() => {
+      mockedAxios.get.mockReset();
+      mockedAxios.get.mockReturnValue(Promise.resolve({}));
+
+      wrapper = shallowMount<BankAccountClass>(BankAccountDetailComponent, {
+        store,
+        i18n,
+        localVue,
+        provide: { bankAccountService: () => new BankAccountService() }
+      });
+      comp = wrapper.vm;
+    });
+
+    describe('OnInit', async () => {
+      it('Should call load all on init', async () => {
+        // GIVEN
+        mockedAxios.get.mockReturnValue(Promise.resolve({ data: { id: 123 } }));
+
+        // WHEN
+        comp.retrieveBankAccount(123);
+        await comp.$nextTick();
+
+        // THEN
+        expect(comp.bankAccount).toEqual(jasmine.objectContaining({ id: 123 }));
+      });
+    });
+  });
+});
